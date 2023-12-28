0e7523d25fe1cf34c0e7f2d1a52492ad35d403a5
meetshaheer <shaheer.ahmed0404@gmail.com>
2023-12-28 13:28:26 +0500

    Update Examples

--- Conditional Statements.dart
+++ Conditional Statements.dart
@@ -3,9 +3,11 @@ void main(List<String> args) {
   // Return Possible outputs basedon your condition/s.
 
   // Single If-Statement.
+  // In Single If Statement if your condition met, it provide output, otherwise no.
 
   int age = 15;
   String gender = "Male";
+  int nmb = 976431;
 
   // Example 1
   if (age > 10) {
@@ -20,5 +22,10 @@ void main(List<String> args) {
   // Example 3
   if (gender == "Male") {
     print("You're Male bro");
+  }
+
+  // Example 4
+  if (nmb % 2 != 0) {
+    print("Congrats. You Enter an Odd Number");
   }
 }
