374f6368d4330dde05c9446e3568d6a876fbf4f2
meetshaheer <shaheer.ahmed0404@gmail.com>
2023-12-28 13:25:49 +0500

    Clarify Single If Statements

--- Conditional Statements.dart
+++ Conditional Statements.dart
@@ -1,4 +1,24 @@
 void main(List<String> args) {
   // Conditional Statements.
   // Return Possible outputs basedon your condition/s.
+
+  // Single If-Statement.
+
+  int age = 15;
+  String gender = "Male";
+
+  // Example 1
+  if (age > 10) {
+    print("Your age is greater then 10");
+  }
+
+  // Example 2
+  if (age == 15) {
+    print("Congrat! You're 15");
+  }
+
+  // Example 3
+  if (gender == "Male") {
+    print("You're Male bro");
+  }
 }
