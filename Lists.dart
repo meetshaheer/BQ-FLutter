a376f0316423bba7112f8e2d4d48c1a91227b626
meetshaheer <shaheer.ahmed0404@gmail.com>
2024-01-02 12:00:12 +0500

    List Specification Using Type Restricion

--- Lists.dart
+++ Lists.dart
@@ -1,1 +1,16 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  // // LISTS
+
+  List studentName = ["Shaheer", "Bilal", "Asad", "Saad"]; // Get all data types
+  List data = ["Shaheer", "Ahmed", 24, "Karachi", "MAJU"]; // Get all data types
+  List students = ["Ali", "Ahmed", "Kamra", 43];
+  print(studentName);
+  print(data);
+
+  // Specify the data type.
+  List<int> Age = [1, 5, 7, 4, 2, 7, 3, 2]; // Only accept int type elements.
+  // List<int> Ages = [1, 5, 7, 4, 2, 7, "Ali"]; // Hold Error
+
+  List<double> height = [1.1, 5.9, 7.6]; // Only accept int type elements.
+  // List<double> heights = ["A", 5.9, 7.6]; // Only accept int type elements.
+}
