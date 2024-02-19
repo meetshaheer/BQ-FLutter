3020809decf8ccb0c29b31d3ca34a3d42f3d5068
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-02-19 12:02:15 +0500

    Defining Synchronus Programming

--- synchronus_programming.dart
+++ synchronus_programming.dart
@@ -1,1 +1,20 @@
+import 'dart:async';
+void main(List<String> args) {
+  // Synchronus Programming
+  // immediately fullfil statements block
 
+  // Asynchronus Programming
+  // Use async functions to add a specific block
+
+  // Synchronus Example
+
+  int a = 2;
+  int b = 3;
+
+  int add = a + b;
+  print(add);
+
+
+  // Asynchronus Example
+  // First import async library
+}
