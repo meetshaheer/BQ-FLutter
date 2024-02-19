6ac574db08d92f38c324748dd8d6c619cfd8a557
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-02-19 09:41:23 +0500

    Defining Enums with an example

--- Enumerated_types.dart
+++ Enumerated_types.dart
@@ -1,4 +1,11 @@
+// Enumerated types, often called enumerations or enums, are a special kind of
+// class used to represent a fixed number of constant values.
+
+// Note:  All Enums implemented in enum class.
+
 enum AccountType { Free, Premium, vip }
+
+enum Color { black, blue }
 
 void main(List<String> args) {
   final userAccountType = AccountType.vip;
@@ -19,5 +26,15 @@ void main(List<String> args) {
 
     default:
       print("Invalid Account Type");
+  }
+
+  // Example:
+
+  final userColor = Color.blue;
+
+  if (userColor == Color.blue) {
+    print("Your Color is Blue");
+  } else {
+    print("Your Color is Black");
   }
 }
