652540a226d1a34ba812e7e0226f11942653a563
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-02-19 12:18:16 +0500

    Defining Asynchronus Programming

--- synchronus_programming.dart
+++ synchronus_programming.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
-void main(List<String> args) {
+import 'dart:io';
+
+void main(List<String> args) async {
   // Synchronus Programming
   // immediately fullfil statements block
 
@@ -12,9 +14,44 @@ void main(List<String> args) {
   int b = 3;
 
   int add = a + b;
-  print(add);
+  // print(add);
 
+  // Asynchronus Example.
+  // First import async library.
+  // Use async method with function.
+  // Then adding Delays using Future and await Functions/ keywords.
 
-  // Asynchronus Example
-  // First import async library
+  print("No Delayed");
+  await Future.delayed(Duration(seconds: 2));
+  print("Delayed\n\n\n");
+
+  // Delay Function Examples
+  // Example 1
+  delayFunction();
+  // Example 2
+  userLogin();
+}
+
+// Example 1
+void delayFunction() async {
+  print("Wait! Your request is processing");
+  await Future.delayed(Duration(seconds: 10));
+  print("Your Request Proceed");
+}
+
+// Example 2
+void userLogin() async {
+  print("Enter YOur User Name");
+  String id = stdin.readLineSync()!;
+
+  print("Enter YOur Password");
+  String password = stdin.readLineSync()!;
+
+  if (password == "abc123") {
+    print("Wait! Matching Credentials....");
+    await Future.delayed(Duration(seconds: 3));
+    print("`````Matched`````\n You can Transact");
+  } else {
+    print("Account Not Found. Enter Correct Credentials");
+  }
 }
